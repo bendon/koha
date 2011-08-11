14001246428fc7bfbe45d9502f395ec89c640cab
Colin Campbell <colin.campbell@ptfs-europe.com>
2011-08-11 10:00:35 +0100

    Bring schema up to 3.5.8 db

--- Koha/Schema/Result/City.pm
+++ Koha/Schema/Result/City.pm
@@ -33,11 +33,23 @@ __PACKAGE__->table("cities");
   is_nullable: 0
   size: 100
 
+=head2 city_state
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 100
+
 =head2 city_zipcode
 
   data_type: 'varchar'
   is_nullable: 1
   size: 20
+
+=head2 city_country
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 100
 
 =cut
 
@@ -46,14 +58,18 @@ __PACKAGE__->add_columns(
   { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
   "city_name",
   { data_type => "varchar", default_value => "", is_nullable => 0, size => 100 },
+  "city_state",
+  { data_type => "varchar", is_nullable => 1, size => 100 },
   "city_zipcode",
   { data_type => "varchar", is_nullable => 1, size => 20 },
+  "city_country",
+  { data_type => "varchar", is_nullable => 1, size => 100 },
 );
 __PACKAGE__->set_primary_key("cityid");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07002 @ 2011-04-26 16:38:49
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:bm6RwEqQBzBoeO2PtxpsRA
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-08-11 09:59:49
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uo6korP7X4iMLR3CnhyQog
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
